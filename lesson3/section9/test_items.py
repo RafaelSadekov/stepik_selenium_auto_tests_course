bcd64b075e7f3ae25b715a1e43250e0304eac525
Rafael <rafael.sadekov@gmail.com>
2022-07-19 12:32:27 +0300

    upd

--- lesson3/section9/test_items.py
+++ lesson3/section9/test_items.py
@@ -4,12 +4,8 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-
-
-
 def test_check_basket_button_exists(browser):
-    browser.get(link)
+    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
     #time.sleep(20)
     WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "btn-add-to-basket")))
     check_basket_button_exists = browser.find_elements(By.CLASS_NAME, "btn-add-to-basket")
