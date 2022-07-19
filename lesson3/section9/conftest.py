4acb148b59f56b8b5b74b0adc9e91c890ba27917
Rafael <rafael.sadekov@gmail.com>
2022-07-19 12:21:57 +0300

    Задание: запуск автотестов для разных языков интерфейса

--- lesson3/section9/conftest.py
+++ lesson3/section9/conftest.py
@@ -1,32 +1,37 @@
 import pytest
 from selenium import webdriver
+# подключаем класс Options для объявления языков
+from selenium.webdriver.chrome.options import Options
+
+
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
+    parser.addoption('--browser_name', action='store', default='chrome',
                      help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose language: es, en, ru, fr")
+
 
 
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
     browser = None
     if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
+        print(f"\nStart browser {browser_name} for test..")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
+        browser.implicitly_wait(10)
     elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
+        print(f"\nStart browser {browser_name} for test..")
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+        browser.implicitly_wait(10)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
     print("\nquit browser..")
     browser.quit()
-
-   # pytest -s -v --browser_name=chrome test_parser.py
-   #pytest -s -v --browser_name=firefox test_parser.py
-
-#Можно задать значение параметра по умолчанию, чтобы в командной строке не обязательно было указывать параметр
-# --browser_name, например, так:
-
-#parser.addoption('--browser_name', action='store', default="chrome",
-#                 help="Choose browser: chrome or firefox")
