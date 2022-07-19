98420ba4daf0689f36a935d36b1b3d947e71fde6
Rafael <rafael.sadekov@gmail.com>
2022-07-19 12:29:21 +0300

    upd

--- lesson3/section9/test_items.py
+++ lesson3/section9/test_items.py
@@ -8,11 +8,11 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
 
-def test_single_add_to_basket_button_should_be_present(browser):
+def test_check_basket_button_exists(browser):
     browser.get(link)
     #time.sleep(20)
     WebDriverWait(browser, 10).until(EC.visibility_of_element_located((By.CLASS_NAME, "btn-add-to-basket")))
-    add_to_basket_button_elements = browser.find_elements(By.CLASS_NAME, "btn-add-to-basket")
-    assert len(add_to_basket_button_elements) == 1
+    check_basket_button_exists = browser.find_elements(By.CLASS_NAME, "btn-add-to-basket")
+    assert len(check_basket_button_exists) == 1
 
 
